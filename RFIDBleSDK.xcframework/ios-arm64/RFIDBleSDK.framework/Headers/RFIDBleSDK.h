640959176df50ea1d39bc6ee327d0913d886e97c
Horseshoe <68278919+zsguang@users.noreply.github.com>
2025-06-04 10:29:51 +0800

    Add files via upload
    sdk

---
+++ RFIDBleSDK.xcframework/ios-arm64/RFIDBleSDK.framework/Headers/RFIDBleSDK.h
@@ -0,0 +1,18 @@
+//
+//  RFIDBleSDK.h
+//  RFIDBleSDK
+//
+//  Created by zsg on 2024/4/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RFIDBleSDK.
+FOUNDATION_EXPORT double RFIDBleSDKVersionNumber;
+
+//! Project version string for RFIDBleSDK.
+FOUNDATION_EXPORT const unsigned char RFIDBleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFIDBleSDK/PublicHeader.h>
+
+
